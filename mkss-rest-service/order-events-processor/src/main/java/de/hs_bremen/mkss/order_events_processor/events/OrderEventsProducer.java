957844bb39a031fe9426e9f32e94292250b7c711
zuzannakk13 <zkwoczek@gmail.com>
2025-01-13 21:56:45 +0100

    Ex. 7.3 -  Sending Reply Events from Consumer Application to Order REST Service Randomly set status of the order & then sending to message broker

---
+++ mkss-rest-service/order-events-processor/src/main/java/de/hs_bremen/mkss/order_events_processor/events/OrderEventsProducer.java
@@ -0,0 +1,69 @@
+package de.hs_bremen.mkss.order_events_processor.events;
+
+import de.hsbremen.mkss.events.CrudEventProducer;
+import de.hsbremen.mkss.events.Event;
+import de.hsbremen.mkss.events.EventWithPayload;
+import de.hsbremen.mkss.events.dto.ItemDTO;
+import de.hsbremen.mkss.events.dto.OrderDTO;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+
+// XYZ replaced with Order
+@Component
+public class OrderEventsProducer implements CrudEventProducer<OrderDTO> {
+
+	private final AmqpTemplate amqpTemplate;
+
+	@Value("${my.rabbitmq.an.exchange}")
+	String orderExchange;
+
+    /*@Value("${my.rabbitmq.a.routing.key}")
+    String orderRoutingKey;*/
+
+
+	public OrderEventsProducer(AmqpTemplate amqpTemplate) {
+		this.amqpTemplate = amqpTemplate;
+	}
+
+	private EventWithPayload<OrderDTO> buildEvent(Event.EventType type, OrderDTO orderDTO) {
+		EventWithPayload<OrderDTO> event = new EventWithPayload.Builder<OrderDTO>()
+				.setType(type)
+				.setPayload(orderDTO)
+				.build();
+		return event;
+	}
+
+	@Override
+	public void emitCreateEvent(OrderDTO orderDTO) {
+		EventWithPayload<OrderDTO> event = buildEvent(Event.EventType.CREATED, orderDTO);
+
+		//  send event to RabbitMQ exchange
+		sendEventToRabbitMQ(event);
+	}
+
+	@Override
+	public void emitUpdateEvent(OrderDTO orderDTO) {
+		// Implementation for update events (e.g. changed order)
+		EventWithPayload<OrderDTO> event = buildEvent(Event.EventType.CHANGED, orderDTO);
+		sendEventToRabbitMQ(event);
+	}
+
+	@Override
+	public void emitDeleteEvent(OrderDTO orderDTO) {
+		// Implementation for delete events (e.g. deleted order)
+		EventWithPayload<OrderDTO> event = buildEvent(Event.EventType.DELETED, orderDTO);
+		sendEventToRabbitMQ(event);
+	}
+
+	private void sendEventToRabbitMQ(EventWithPayload<OrderDTO> event) {
+		amqpTemplate.convertAndSend(orderExchange, "", event);
+		// routing key is empty string because of fanout
+		System.out.println("Sent event = " + event + " using exchange " + orderExchange);
+	}
+}
--- mkss-rest-service/order-events-processor/src/main/java/de/hs_bremen/mkss/order_events_processor/service/OrderEventsConsumer.java
+++ mkss-rest-service/order-events-processor/src/main/java/de/hs_bremen/mkss/order_events_processor/service/OrderEventsConsumer.java
@@ -1,17 +1,42 @@
 package de.hs_bremen.mkss.order_events_processor.service;
 
+import de.hs_bremen.mkss.order_events_processor.events.OrderEventsProducer;
 import jakarta.annotation.PostConstruct;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
 import de.hsbremen.mkss.events.EventWithPayload;
 import de.hsbremen.mkss.events.dto.OrderDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderEventsConsumer {
+    private final OrderEventsProducer orderEventsProducer;
+
+    @Autowired
+    public OrderEventsConsumer(OrderEventsProducer orderEventsProducer) {
+        this.orderEventsProducer = orderEventsProducer;
+    }
 
     @RabbitListener(queues = "orderQueue")
     public void receiveMessage(EventWithPayload<OrderDTO> event) {
         System.out.println("Received Order Event: " + event);
+        var order = event.getPayload();
+
+        if (order.getStatus() != "COMMITTED") return;
+
+        if (getRandomBoolean()) {
+            order.setStatus("ACCEPTED");
+        } else {
+            order.setStatus("REJECTED");
+        }
+
+        orderEventsProducer.emitUpdateEvent(order);
+    }
+
+    private static boolean getRandomBoolean() {
+        return Math.random() < 0.5;
     }
 }
